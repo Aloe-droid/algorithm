af68e8439157cd81dd9e55f8ee6cdc5326dee1bf
Aloe-droid <103430477+Aloe-droid@users.noreply.github.com>
2024-03-23 21:37:06 +0900

    [Gold III] Title: 내리막 길, Time: 372 ms, Memory: 34344 KB -BaekjoonHub

---
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int M;
+    static int[][] ints;
+    static int[][] dp;
+    static int[] dx = {1, -1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        ints = new int[N][M];
+        dp = new int[N][M];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                ints[i][j] = Integer.parseInt(st.nextToken());
+                dp[i][j] = -1;
+            }
+        }
+        dp[N - 1][M - 1] = 1;
+        System.out.println(dfs(0, 0));
+    }
+
+    public static int dfs(int x, int y) {
+        if (dp[x][y] != -1) return dp[x][y];
+
+        dp[x][y] = 0;
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx < 0 || nx >= N || ny < 0 || ny >= M || ints[x][y] <= ints[nx][ny]) continue;
+            dp[x][y] += dfs(nx, ny);
+        }
+
+        return dp[x][y];
+    }
+}
