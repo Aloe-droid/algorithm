bbf5b75279edf0bb609d0b854b58774d9e6d5e46
Aloe-droid <103430477+Aloe-droid@users.noreply.github.com>
2024-03-27 15:55:01 +0900

    [D6] Title: [S/W 문제해결 응용] 8일차 - 사람 네트워크2, Time: 1,398 ms, Memory: 108,468 KB -BaekjoonHub

--- SWEA/D6/1263. ［S／W 문제해결 응용］ 8일차 － 사람 네트워크2/［S／W 문제해결 응용］ 8일차 － 사람 네트워크2.java
+++ SWEA/D6/1263. ［S／W 문제해결 응용］ 8일차 － 사람 네트워크2/［S／W 문제해결 응용］ 8일차 － 사람 네트워크2.java
@@ -1,35 +1,41 @@
 import java.io.*;
 import java.util.*;
- 
+
 class Solution {
-    static int MAX = 100_000_000;
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int T = Integer.parseInt(br.readLine());
-        for (int tc = 1; tc <= T; tc++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int N = Integer.parseInt(st.nextToken());
-            int[][] ints = new int[N][N];
-            for (int i = 0; i < N; i++) {
-                for(int j = 0; j < N; j++) {
-                    ints[i][j] = Integer.parseInt(st.nextToken());
-                    if (i != j && ints[i][j] == 0) ints[i][j] = MAX;
-                }
-            }
-             
-            for (int k = 0; k < N; k++) {
-                for (int i = 0; i < N; i++) {
-                    for (int j = 0; j < N; j++) {
-                        ints[i][j] = Math.min(ints[i][j], ints[i][k] + ints[k][j]);
-                    }
-                }
-            }
-             
-            int min = Integer.MAX_VALUE;
-            for (int i = 0; i < N; i++) min = Math.min(min, Arrays.stream(ints[i]).sum());
-            sb.append("#").append(tc).append(" ").append(min).append("\n");
-        }
-        System.out.println(sb);
-    }
+	static int MAX = 100_000_000;
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int[][] ints = new int[N][N];
+			for (int i = 0; i < N; i++) {
+				for(int j = 0; j < N; j++) {
+					ints[i][j] = Integer.parseInt(st.nextToken());
+					if (i != j && ints[i][j] == 0) ints[i][j] = MAX;
+				}
+			}
+			
+			for (int k = 0; k < N; k++) {
+				for (int i = 0; i < N - 1; i++) {
+					for (int j = i + 1; j < N; j++) {
+						int sum = ints[i][k] + ints[k][j];
+						ints[i][j] = ints[i][j] > sum ? sum : ints[i][j];
+					}
+				}
+			}
+			
+			int min = Integer.MAX_VALUE;
+			for (int i = 0; i < N; i++) {
+				int temp = 0;
+				for (int j = N - 1; j >= 0; j--) temp += j >= i ? ints[i][j] : ints[j][i];
+				min = min > temp ? temp : min;
+			}
+			
+			sb.append("#").append(tc).append(" ").append(min).append("\n");
+		}
+		System.out.println(sb);
+	}
 }
