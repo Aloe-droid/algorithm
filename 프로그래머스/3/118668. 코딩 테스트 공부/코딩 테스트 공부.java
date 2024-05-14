ff585915c765c500c58a1e829d6dc765a0b3fc33
Aloe-droid <103430477+Aloe-droid@users.noreply.github.com>
2024-05-14 18:27:42 +0900

    [level 3] Title: 코딩 테스트 공부, Time: 1.67 ms, Memory: 52.2 MB -BaekjoonHub

--- 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.java
+++ 프로그래머스/3/118668. 코딩 테스트 공부/코딩 테스트 공부.java
@@ -1,67 +1,41 @@
 import java.util.*;
 
 class Solution {
-    static int maxX, maxY;
-    static int[][] maps;
-    static Queue<int[]> q;
-    static int[][] problems;
+    static int[][] dp;
+    static int X,Y;
     
     public int solution(int alp, int cop, int[][] problems) {
-        maxX = alp;
-        maxY = cop;
-        Solution.problems = problems;
+        X = alp;
+        Y = cop;
         
-        // 가로: 알고력, 세로: 코딩력
         for(int[] problem : problems){
-            if(problem[0] > maxX) maxX = problem[0];
-            if(problem[1] > maxY) maxY = problem[1];
+            X = Math.max(X, problem[0]);
+            Y = Math.max(Y, problem[1]);
         }
-        
-        maps = new int[maxX + 1][maxY + 1];
-        init(alp, cop);
+        dp = new int[X + 1][Y + 1];
         
         
-        for(int i = alp; i <= maxX; i++){
-            for(int j = cop; j <= maxY; j++){
-                bfs(i, j);
+        for(int i = alp; i <= X; i++){
+            for(int j = cop; j <= Y; j++){
+                dp[i][j] = (i - alp) + (j - cop);
             }
         }
-        return maps[maxX][maxY];
-    }
-    
-    public static void init(int x, int y){
-        for(int i = x; i <= maxX; i++){
-            for(int j = y; j <= maxY; j++){
-                maps[i][j] = Math.abs(i - x) + Math.abs(j - y);
-            }
-        }
-    }
-    
-    public static void bfs(int x, int y){
-        q = new LinkedList<>();
-        q.add(new int[] {x, y});
         
-        while(!q.isEmpty()){
-            int px = q.peek()[0];
-            int py = q.poll()[1];
-            
-            for(int[] problem : problems){
-                int nx = px + problem[2];
-                int ny = py + problem[3];
+        
+        for(int i = alp; i <= X; i++){
+            for(int j = cop; j <= Y; j++){
                 
-                if(nx > maxX) nx = maxX;
-                if(ny > maxY) ny = maxY;
-                
-                int alpha = 0;
-                if(px < problem[0]) alpha += problem[0] - px;
-                if(py < problem[1]) alpha += problem[1] - py;
-                alpha += problem[4];
-                
-                if(maps[nx][ny] > maps[px][py] + alpha){
-                    maps[nx][ny] = maps[px][py] + alpha;
-                    q.add(new int[] {nx, ny});
+                for(int[] problem : problems){
+                    if(problem[0] > i || problem[1] > j) continue;
+                    
+                    int nx = i + problem[2] < X ? i + problem[2] : X;
+                    int ny = j + problem[3] < Y ? j + problem[3] : Y;
+                    
+                    int temp = dp[i][j] + problem[4];
+                    dp[nx][ny] = Math.min(dp[nx][ny], temp);
                 }
             }
         }
+        return dp[X][Y];
     }
 }
