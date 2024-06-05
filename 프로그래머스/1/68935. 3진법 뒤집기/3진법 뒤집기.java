c5d1d54c3452392c107051a46aaa78524bd75966
Aloe-droid <103430477+Aloe-droid@users.noreply.github.com>
2024-06-05 17:50:55 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.04 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -8,14 +8,6 @@ class Solution {
             n /= 3;
         }
         
-        String s = sb.toString();
-        int ans = 0;
-        for(int i = 0; i < s.length(); i++){
-            int k = s.charAt(i) - '0';
-            if(k == 0) continue;
-            
-            ans += k * (int) Math.pow(3, s.length() - 1 - i);
-        }
-        return ans;
+        return Integer.parseInt(sb.toString(), 3);
     }
 }
