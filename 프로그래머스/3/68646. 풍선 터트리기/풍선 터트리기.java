6401d051ee571c4392f2477e20df5a52c8abef16
Aloe-droid <103430477+Aloe-droid@users.noreply.github.com>
2024-06-10 09:34:10 +0900

    [level 3] Title: 풍선 터트리기, Time: 303.98 ms, Memory: 195 MB -BaekjoonHub

--- 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
+++ 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.java
@@ -3,15 +3,21 @@ import java.util.*;
 class Solution {
     public int solution(int[] a) {
         HashSet<Integer> hs = new HashSet<>();
-        int i1 = Integer.MAX_VALUE;
-        int i2 = Integer.MAX_VALUE;
+        TreeSet<Integer> ts1 = new TreeSet<>();
+        TreeSet<Integer> ts2 = new TreeSet<>();
         
-        for(int i = 0; i < a.length; i++){
-            i1 = Math.min(i1, a[i]);
-            i2 = Math.min(i2, a[a.length - 1 - i]);
+        ts1.add(a[0]);
+        for(int i = 1; i < a.length; i++) ts2.add(a[i]);
+        
+        hs.add(ts1.first());
+        hs.add(ts2.first());
+        
+        for(int i = 1; i < a.length - 1; i++){
+            ts1.add(a[i]);
+            ts2.remove(a[i]);
             
-            hs.add(i1);
-            hs.add(i2);
+            hs.add(ts1.first());
+            hs.add(ts2.first());
         }
         return hs.size();
     }
