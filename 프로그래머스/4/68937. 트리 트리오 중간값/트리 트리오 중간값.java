3422e91f298f2b6064a581ccc9c32b3aa589f114
Aloe-droid <103430477+Aloe-droid@users.noreply.github.com>
2024-06-13 11:31:26 +0900

    [level 4] Title: 트리 트리오 중간값, Time: 309.06 ms, Memory: 225 MB -BaekjoonHub

--- 프로그래머스/4/68937. 트리 트리오 중간값/트리 트리오 중간값.java
+++ 프로그래머스/4/68937. 트리 트리오 중간값/트리 트리오 중간값.java
@@ -7,9 +7,7 @@ class Solution {
     static int[] check;
     
     public int solution(int n, int[][] edges) {
-        v1 = -1;
-        v2 = -1;
-        v3 = -1;
+        v1 = -1; v2 = - 1; v3 = -1;
         Solution.n = n;
         list = new ArrayList<>();
         for(int i = 0; i <= n; i++) list.add(new ArrayList<>());
@@ -18,40 +16,36 @@ class Solution {
             list.get(edge[1]).add(edge[0]);
         }
         
-        List<Integer> ans = new ArrayList<>();
+        int[] ans = new int[3];
         // 임의의 한 점에서 가장 먼 거리에 있는 노드 
         bfs(1);
         v1 = find(1);
         
         bfs(v1);
         v2 = find(v1);
-        ans.add(check[v2] - 1);
+        ans[0] = check[v2] - 1;
         
         bfs(v2);
         v3 = find(v1);
-        ans.add(check[v3] - 1);
+        ans[1] = check[v3] - 1;
         
         bfs(v3);
-        ans.add(check[v1] - 1);
+        ans[2] = check[v1] - 1;
         
-        Collections.sort(ans);
-        return ans.get(1);
+        Arrays.sort(ans);
+        return ans[1];
     }
     
     public static void bfs(int start){
         q = new LinkedList<>();
         check = new int[n + 1];
-        
         q.add(start);
         check[start] = 1;
         
-        int last = -1;
         while(!q.isEmpty()){
             int x = q.poll();
-            
             for(int nx : list.get(x)){
                 if(check[nx] != 0) continue;
-                
                 q.add(nx);
                 check[nx] = check[x] + 1;
             }
@@ -59,8 +53,7 @@ class Solution {
     }
     
     public static int find(int k){
-        int idx = -1;
-        int max = -1;
+        int idx = -1; int max = -1;
         for(int i = 0; i < check.length; i++){
             if(i == k) continue;
             
