b29333f5fc4d910152d61395641ea694212485e2
Aloe-droid <103430477+Aloe-droid@users.noreply.github.com>
2024-07-03 23:57:41 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 14.56 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -3,9 +3,9 @@ class Solution {
         boolean[] bools = new boolean[1000 * 1000 + 1];
         
         for(int i = 0; i < elements.length; i++){
-            for(int j = 1; j <= elements.length; j++){
-                int sum = 0;
-                for(int k = i; k < i + j; k++) sum += elements[k % elements.length];
+            int sum = 0;
+            for(int j = i; j < i + elements.length; j++){
+                sum += elements[j % elements.length];
                 bools[sum] = true;
             }
         }
