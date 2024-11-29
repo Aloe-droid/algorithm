baced8a4d99c1b47469a477906996d079afb1c80
Aloe-droid <103430477+Aloe-droid@users.noreply.github.com>
2024-11-29 13:50:37 +0900

    [level 2] Title: 방문 길이, Time: 22.22 ms, Memory: 88.8 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -1,54 +1,36 @@
 import java.util.*;
 
 class Solution {
-    public static int[] dx = {1,-1, 0, 0};
-    public static int[] dy = {0, 0,-1, 1};
+    static int[] dx = { 1,-1, 0, 0};
+    static int[] dy = { 0, 0,-1, 1};
     
-    public int solution(String dirs) throws Exception {
-        Map<Integer, Set<Integer>> hm = new HashMap<>();
-        int N = 11;
-        int x = 5, y = 5;
+    public int solution(String dirs) {
+        Set<String> hs = new HashSet<>();
+        int x = 0, y = 0;
         
         for(int i = 0; i < dirs.length(); i++){
             char c = dirs.charAt(i);
-            int idx = -1;
+            int k = 0;
             
-            if(c == 'U') idx = 0;
-            else if(c == 'D') idx = 1;
-            else if(c == 'L') idx = 2;
-            else if(c == 'R') idx = 3;
+            if(c == 'U') k = 0;
+            else if(c == 'D') k = 1;
+            else if(c == 'L') k = 2;
+            else if(c == 'R') k = 3;
             
-            if(idx == -1) throw new RuntimeException("방향 에러");
+            int nx = x + dx[k];
+            int ny = y + dy[k];
+            if(nx < -5 || nx > 5 || ny < -5 || ny > 5) continue;
             
-            int nx = x + dx[idx];
-            int ny = y + dy[idx];
-            
-            if(nx < 0 || nx >= N || ny < 0 || ny >= N ) continue;
-            
-            int key1 = x * N + y;
-            int key2 = nx * N + ny;
-            
-            Set<Integer> hs1 = hm.getOrDefault(key1, new HashSet<>());
-            Set<Integer> hs2 = hm.getOrDefault(key2, new HashSet<>());
-            hs1.add(key2);
-            hs2.add(key1);
-            hm.put(key1, hs1);
-            hm.put(key2, hs2);
+            String s1 = x + ", " + y + " -> " + nx + ", " + ny;
+            String s2 = nx + ", " + ny + " -> " + x + ", " + y;
+            hs.add(s1);
+            hs.add(s2);
             
             x = nx;
             y = ny;
         }
         
-       for(int key : hm.keySet()){
-           for(int value : hm.get(key)){
-               if(hm.containsKey(value)){
-                   hm.get(value).remove(key);
-               }
-           }
-       }
         
-        int count = 0;
-        for(int key : hm.keySet()) count += hm.get(key).size();
-        return count;
+        return hs.size() / 2;
     }
 }
