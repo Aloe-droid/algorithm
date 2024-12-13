32680867064d472995bd1388b01e2f2860a71877
Aloe-droid <103430477+Aloe-droid@users.noreply.github.com>
2024-12-13 15:50:44 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 748 ms, Memory: 123012 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -12,6 +12,7 @@ class Main {
         int[][] ints = new int[N + 1][N + 1];
         int[][] dp = new int[N + 1][N + 1];
 
+        // 입력 받기
         for (int i = 1; i <= N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 1; j <= N; j++) {
@@ -19,13 +20,14 @@ class Main {
             }
         }
 
+        // 2차원 누적합 계산
         for (int i = 1; i <= N; i++) {
             for (int j = 1; j <= N; j++) {
-                dp[i][j] = ints[i][j];
-                if (j - 1 >= 0) dp[i][j] += dp[i][j - 1];
+                dp[i][j] = ints[i][j] + dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1];
             }
         }
 
+        // 쿼리 처리
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int x1 = Integer.parseInt(st.nextToken());
@@ -33,9 +35,8 @@ class Main {
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
 
-            int temp = 0;
-            for (int j = x1; j <= x2; j++) temp += dp[j][y2] - dp[j][y1 - 1];
-            sb.append(temp).append("\n");
+            int result = dp[x2][y2] - dp[x2][y1-1] - dp[x1-1][y2] + dp[x1-1][y1-1];
+            sb.append(result).append("\n");
         }
         System.out.println(sb);
     }
