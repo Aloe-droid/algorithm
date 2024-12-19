194856436c47cfacfcdc737c819a65fbac7eeb8c
Aloe-droid <103430477+Aloe-droid@users.noreply.github.com>
2024-12-20 01:34:58 +0900

    [Gold V] Title: LCS, Time: 144 ms, Memory: 18648 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.java
+++ 백준/Gold/9251. LCS/LCS.java
@@ -5,14 +5,25 @@ class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s1 = br.readLine();
         String s2 = br.readLine();
-        int[][] dp = new int[s1.length() + 1][s2.length() + 1];
+        int n = s1.length();
+        int m = s2.length();
+        int[][] dp = new int[n][m];
 
-        for (int i = 1; i <= s1.length(); i++) {
-            for (int j = 1; j <= s2.length(); j++) {
-                if (s1.charAt(i - 1) == s2.charAt(j - 1)) dp[i][j] = dp[i - 1][j - 1] + 1;
-                else dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(s1.charAt(i) == s2.charAt(j)){
+                    int prev = 0;
+                    if(i - 1 >= 0 && j - 1 >= 0) prev += dp[i - 1][j - 1];
+                    dp[i][j] = prev + 1;
+                }else{
+                    int max = 0;
+                    if(i - 1 >= 0) max = Math.max(max, dp[i - 1][j]);
+                    if(j - 1 >= 0) max = Math.max(max, dp[i][j - 1]);
+                    dp[i][j] = max;
+                }
             }
         }
-        System.out.println(dp[s1.length()][s2.length()]);
+
+        System.out.println(dp[n - 1][m - 1]);
     }
 }
