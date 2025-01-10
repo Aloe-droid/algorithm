4e4115d1e4257759f5e266dd710143accbb46485
Aloe-droid <103430477+Aloe-droid@users.noreply.github.com>
2025-01-10 22:06:11 +0900

    [level 2] Title: 귤 고르기, Time: 524.88 ms, Memory: 403 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -2,24 +2,21 @@ import java.util.*;
 
 class Solution {
     Guul[] guuls;
-    public int solution(int k, int[] tangerine) {
-        int size = tangerine[0];
-        for(int t : tangerine) size = Math.max(size, t);
+    public int solution(int k, int[] ints) {
+        int size = ints[0];
+        for(int t : ints) size = Math.max(size, t);
         guuls = new Guul[size + 1];
         for(int i = 0; i <= size; i++) guuls[i] = new Guul(i);
-        for(int t : tangerine) guuls[t].cnt += 1;
+        for(int t : ints) guuls[t].cnt += 1;
         Arrays.sort(guuls, Comparator.comparingInt(g -> g.cnt));
         
         int now = 0;
         int ans = 0;
-        
         for(int i = size; i >= 0; i--) {
             now += guuls[i].cnt;
             ans += 1;
-            
             if(now >= k) break;
         }
-        
         return ans;
     }
 }
