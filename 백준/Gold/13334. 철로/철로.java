2f057c218ff850c4db000cfa9ccf9c110c64c810
Aloe-droid <103430477+Aloe-droid@users.noreply.github.com>
2025-01-17 22:22:50 +0900

    [Gold II] Title: 철로, Time: 660 ms, Memory: 49304 KB -BaekjoonHub

--- 백준/Gold/13334. 철로/철로.java
+++ 백준/Gold/13334. 철로/철로.java
@@ -3,12 +3,11 @@ import java.util.*;
 
 class Main {
     static int N, L;
-    static K[] kH, kO;
+    static K[] ks;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        kH = new K[N];
-        kO = new K[N];
+        ks = new K[N];
 
         for(int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
@@ -16,64 +15,27 @@ class Main {
             int t2 = Integer.parseInt(st.nextToken());
             int h = Math.min(t1, t2);
             int o = Math.max(t1, t2);
-            kH[i] = new K(i, h);
-            kO[i] = new K(i, o);
+            ks[i] = new K(h, o);
         }
         L = Integer.parseInt(br.readLine());
-        Arrays.sort(kH, Comparator.comparingInt(k -> k.v));
-        Arrays.sort(kO, Comparator.comparingInt(k -> k.v));
+        Arrays.sort(ks, Comparator.comparingInt(k -> k.o));
 
-        int max = 0, cnt = 0;
-        int[] ints = new int[N];
-
-        int h1 = 0, h2 = 0;
-        int o1 = 0, o2 = 0;
-        int index = 0;
-        while(index < N) {
-            int st = kH[index].v, ed = st + L;
-
-            while(h1 < h2 && kH[h1].v < st) {
-                if(ints[kH[h1].i] == 2) cnt -= 1;
-                ints[kH[h1].i] -= 1;
-                h1 += 1;
-            }
-
-            while(h2 < N && kH[h2].v <= ed) {
-                ints[kH[h2].i] += 1;
-                if(ints[kH[h2].i] == 2) cnt += 1;
-                h2 += 1;
-            }
-
-            while(o1 < o2 && kO[o1].v < st) {
-                if(ints[kO[o1].i] == 2) cnt -= 1;
-                ints[kO[o1].i] -= 1;
-                o1 += 1;
-            }
-
-            while(o2 < N && kO[o2].v <= ed) {
-                ints[kO[o2].i] += 1;
-                if(ints[kO[o2].i] == 2) cnt += 1;
-                o2 += 1;
-            }
-
-            if(cnt > 0) max = Math.max(max, cnt);
-            while(index < N && kH[index].v == st) index++;
+        int max = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i = 0; i < N; i++) {
+            pq.add(ks[i].h);
+            while(!pq.isEmpty() && pq.peek() < ks[i].o - L) pq.poll();
+            max = Math.max(max, pq.size());
         }
-
         System.out.println(max);
     }
 }
 
 class K {
-    int i, v;
+    int h, o;
 
-    public K(int i, int v) {
-        this.i = i;
-        this.v = v;
-    }
-
-    @Override
-    public String toString() {
-        return "id: " + i  + ", value: " + v;
+    public K(int h, int o) {
+        this.h = h;
+        this.o = o;
     }
 }
