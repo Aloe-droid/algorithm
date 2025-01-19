8842a13df803806771ac69536749e724ea7294e5
Aloe-droid <103430477+Aloe-droid@users.noreply.github.com>
2025-01-20 01:42:14 +0900

    [Gold IV] Title: 카우버거 알바생, Time: 160 ms, Memory: 17492 KB -BaekjoonHub

---
+++ 백준/Gold/17208. 카우버거 알바생/카우버거 알바생.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    static int N, M, K, max = 0;
+    static int[][] dp;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        dp = new int[M + 1][K + 1];
+        for(int i = 0 ; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            for(int x = M; x >= a; x--){
+                for(int y = K; y >= b; y--) {
+                    dp[x][y] = Math.max(dp[x][y], dp[x - a][y - b] + 1);
+                }
+            }
+        }
+
+        for(int[] d : dp) for(int p : d) max = Math.max(max, p);
+        System.out.println(max);
+    }
+
+}
