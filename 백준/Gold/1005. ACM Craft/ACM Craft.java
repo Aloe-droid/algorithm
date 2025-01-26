472f0fa544c70df7b0407763cb7c1fd2d4f314c6
Aloe-droid <103430477+Aloe-droid@users.noreply.github.com>
2025-01-26 17:09:28 +0900

    [Gold III] Title: ACM Craft, Time: 668 ms, Memory: 240544 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.java
+++ 백준/Gold/1005. ACM Craft/ACM Craft.java
@@ -1,61 +1,46 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
-public class Main {
-    static int N;
-    static int[] delays;
-    static int[] inDegrees;
-    static int[] dp;
+class Main {
+    static int N, K, W;
+    static int[] times, dp;
     static List<List<Integer>> list;
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
-
-        for (int tc = 1; tc <= T; tc++) {
+        int T = Integer.parseInt(br.readLine());
+        for(int tc = 1; tc <= T; tc++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             N = Integer.parseInt(st.nextToken());
-            int K = Integer.parseInt(st.nextToken());
-            delays = new int[N + 1];
-            inDegrees = new int[N + 1];
+            K = Integer.parseInt(st.nextToken());
+            times = new int[N + 1];
             dp = new int[N + 1];
             list = new ArrayList<>();
-            for (int i = 0; i <= N; i++) list.add(new ArrayList<>());
+            for(int i = 0; i <= N; i++) list.add(new ArrayList<>());
             st = new StringTokenizer(br.readLine());
-            for (int i = 1; i <= N; i++) delays[i] = Integer.parseInt(st.nextToken());
-
-            for (int i = 0; i < K; i++) {
+            for(int i = 1; i <= N; i++) times[i] = Integer.parseInt(st.nextToken());
+            for(int i = 0; i < K; i++) {
                 st = new StringTokenizer(br.readLine());
                 int x = Integer.parseInt(st.nextToken());
                 int y = Integer.parseInt(st.nextToken());
-                list.get(x).add(y);
-                inDegrees[y]++;
+                list.get(y).add(x);
             }
-            int end = Integer.parseInt(br.readLine());
+            W = Integer.parseInt(br.readLine());
 
-            Queue<Integer> q = new LinkedList<>();
-            for (int i = 1; i <= N; i++) {
-                if (inDegrees[i] == 0) {
-                    q.add(i);
-                    dp[i] = delays[i];
-                }
-            }
+            Arrays.fill(dp, -1);
+            for(int i = 1; i <= N; i++) if(list.get(i).size() == 0) dp[i] = times[i];
 
-            while (!q.isEmpty()) {
-                int x = q.poll();
-                for (int nx : list.get(x)) {
-                    inDegrees[nx]--;
-                    dp[nx] = Math.max(dp[nx], dp[x] + delays[nx]);
-                    if (inDegrees[nx] == 0) {
-                        q.add(nx);
-                    }
-                }
-            }
-            sb.append(dp[end]).append("\n");
-
+            sb.append(dfs(W)).append("\n");
         }
         System.out.println(sb);
     }
+
+    public static int dfs(int id) {
+        if(dp[id] != -1) return dp[id];
+
+        dp[id] = 0;
+        for(int next : list.get(id)) dp[id] = Math.max(dp[id], dfs(next) + times[id]);
+        return dp[id];
+    }
 }
+
