e158e8228a6fcd0586e553066d208a6fdcc5dfe3
Aloe-droid <103430477+Aloe-droid@users.noreply.github.com>
2025-03-13 01:19:48 +0900

    [Gold IV] Title: 월드컵, Time: 100 ms, Memory: 14144 KB -BaekjoonHub

--- 백준/Gold/6987. 월드컵/월드컵.java
+++ 백준/Gold/6987. 월드컵/월드컵.java
@@ -1,10 +1,11 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 class Main {
     static boolean flag;
     static int N = 6, M = 3;
-    static int[][] results, ints;
+    static int[][] results;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
@@ -12,15 +13,27 @@ class Main {
         for(int i = 0; i < 4; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             results = new int[N][M];
-            ints = new int[N][M];
             flag = false;
 
+            boolean temp = false;
             for(int n = 0; n < N; n++) {
+                int sum = 0;
                 for(int m = 0; m < M; m++) {
                     results[n][m] = Integer.parseInt(st.nextToken());
+                    sum += results[n][m];
+                }
+
+                if(sum != 5) {
+                    temp = true;
+                    break;
                 }
             }
 
+            if(temp) {
+                sb.append("0 ");
+                continue;
+            }
+            
             dfs(0, 1);
             sb.append(flag ? 1 : 0).append(" ");
         }
@@ -29,9 +42,8 @@ class Main {
 
     public static void dfs(int x, int nx) {
         if(flag) return;
-
         if(x == N - 1) {
-            check();
+            flag = true;
             return;
         }
 
@@ -40,32 +52,29 @@ class Main {
             return;
         }
 
-        ints[x][0] += 1;
-        ints[nx][2] += 1;
-        if(!flag) dfs(x, nx + 1);
-        ints[x][0] -= 1;
-        ints[nx][2] -= 1;
-
-        ints[x][2] += 1;
-        ints[nx][0] += 1;
-        if(!flag) dfs(x, nx + 1);
-        ints[x][2] -= 1;
-        ints[nx][0] -= 1;
-
-        ints[x][1] += 1;
-        ints[nx][1] += 1;
-        if(!flag) dfs(x, nx + 1);
-        ints[x][1] -= 1;
-        ints[nx][1] -= 1;
-    }
-
-    public static void check() {
-        for(int i = 0; i < N; i++) {
-            for(int j = 0; j < 3; j++) {
-                if(results[i][j] != ints[i][j]) return;
-            }
+        if(results[x][0] > 0 && results[nx][2] > 0) {
+            results[x][0] -= 1;
+            results[nx][2] -= 1;
+            if(!flag) dfs(x, nx + 1);
+            results[x][0] += 1;
+            results[nx][2] += 1;
         }
 
-        flag = true;
+        if(results[x][2] > 0 && results[nx][0] > 0) {
+            results[x][2] -= 1;
+            results[nx][0] -= 1;
+            if(!flag) dfs(x, nx + 1);
+            results[x][2] += 1;
+            results[nx][0] += 1;
+        }
+
+        if(results[x][1] > 0 && results[nx][1] > 0 ) {
+            results[x][1] -= 1;
+            results[nx][1] -= 1;
+            if(!flag) dfs(x, nx + 1);
+            results[x][1] += 1;
+            results[nx][1] += 1;
+        }
     }
+
 }
