e09b99003adf770b1373d09ff9f3fc80cb5061c5
Aloe-droid <103430477+Aloe-droid@users.noreply.github.com>
2025-03-17 17:00:35 +0900

    [level 3] Title: 다단계 칫솔 판매, Time: 5656.63 ms, Memory: 138 MB -BaekjoonHub

---
+++ 프로그래머스/3/77486. 다단계 칫솔 판매/다단계 칫솔 판매.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class Solution {
+    static Map<String, Seller> hm;
+    
+    public int[] solution(String[] enroll, String[] referral, String[] seller, int[] amount) {
+        init(enroll, referral, seller, amount);
+        return print(enroll);
+    }
+    
+    public static void init(String[] enroll, String[] referral, String[] seller, int[] amount) {
+        hm = new HashMap<>();
+        hm.put("-", new Seller("-"));
+        
+        for(int i = 0; i < enroll.length; i++) {
+            String er = enroll[i];
+            String rf = referral[i];
+            Seller sr = new Seller(er);
+            sr.ref = hm.get(rf);
+            hm.put(er, sr);
+        }
+        
+        for(int i = 0; i < seller.length; i++) hm.get(seller[i]).setPrice(amount[i] * 100);
+    }
+    
+    public static int[] print(String[] enroll) {
+        int[] price = new int[enroll.length];
+        for(int i = 0; i < enroll.length; i++) price[i] = hm.get(enroll[i]).total;
+        return price;
+    }
+}
+
+class Seller {
+    String name;
+    Seller ref;
+    int total;
+    
+    public Seller(String name) {
+        this.name = name;
+        total = 0;
+    }
+    
+    public void setPrice(int price) {
+        int tax = price / 10;
+        total += price - tax;
+        if(ref != null) ref.setPrice(tax);
+    }
+}
