101a4625c078e455bfe28160259f4ff819291b0e
Aloe-droid <103430477+Aloe-droid@users.noreply.github.com>
2025-03-18 12:09:17 +0900

    [level 3] Title: 야근 지수, Time: 134.86 ms, Memory: 70.7 MB -BaekjoonHub

--- 프로그래머스/3/12927. 야근 지수/야근 지수.java
+++ 프로그래머스/3/12927. 야근 지수/야근 지수.java
@@ -1,67 +1,18 @@
 import java.util.*;
 
 class Solution {
-    int n;
-    int[] works;
-    Map<Integer, Integer> tm;
-    
     public long solution(int n, int[] works) {
-        this.n = n;
-        this.works = works;
-        int m = search();
-        return get(m);
-    }
-    
-    public int search() {
-        int left = 0, right = 50000;
-        while(left <= right) {
-            int mid = (left + right) / 2;
-            
-            if(check(mid)) right = mid - 1;
-            else left = mid + 1;
-        }
-        return left;
-    }
-    
-    public boolean check(int m) {
-        int sum = 0;
-        for(int work : works) {
-            if(work > m) sum += work - m;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int work : works) pq.add(work);
+        
+        while(!pq.isEmpty() && n > 0) {
+            int w = pq.poll();
+            n -= 1;
+            if(w - 1 > 0) pq.add(w - 1);
         }
         
-        return sum <= n;
-    }
-    
-    public long get(int m) {
-        int s = 0;
-        tm = new TreeMap<>(Collections.reverseOrder());
-        for(int work : works) {
-            if(work > m) {
-                s += work - m;
-                tm.put(m, tm.getOrDefault(m, 0) + 1);
-            }else{
-                tm.put(work, tm.getOrDefault(work, 0) + 1);
-            }
-        }
-        
-        n -= s;
-    
-        for(int key : tm.keySet()) {
-            if(n == 0) break;
-            
-            int value = tm.get(key);
-            if(value > n) {
-                tm.put(key, value - n);
-                if(key - 1 > 0) tm.put(key - 1, tm.getOrDefault(key - 1, 0) + n);
-                break;
-            }else {
-                n -= value;
-                tm.remove(key);
-            }
-        }
-        
-        long ans = 0L;
-        for(int key : tm.keySet()) ans += (long) Math.pow(key, 2) * tm.get(key);
+        long ans = 0;
+        while(!pq.isEmpty()) ans += (long) Math.pow(pq.poll(), 2);
         return ans;
     }
 }
