a61f0c4005bf64f4a1579cae017a6842a3706f94
Aloe-droid <103430477+Aloe-droid@users.noreply.github.com>
2025-03-18 21:56:40 +0900

    [level 4] Title: 쿠키 구입, Time: 21.45 ms, Memory: 54.7 MB -BaekjoonHub

--- 프로그래머스/4/49995. 쿠키 구입/쿠키 구입.java
+++ 프로그래머스/4/49995. 쿠키 구입/쿠키 구입.java
@@ -2,19 +2,14 @@ class Solution {
     public int solution(int[] cookie) {
         int max = 0, N = cookie.length;
         for(int mid = 0; mid < N - 1; mid++) {
-            boolean[] visit = new boolean[N];
-            int i1 = mid;
-            int i2 = mid + 1;
-            
-            int l = 0, r = 0;
+            int i1 = mid, i2 = mid + 1;
+            int l = cookie[i1], r = cookie[i2];
             while(i1 >= 0 && i2 < N) {
-                l += visit[i1] ? 0 : cookie[i1];
-                r += visit[i2] ? 0 : cookie[i2];
-                visit[i1] = true; visit[i2] = true;
                 if(l == r) max = Math.max(max, l);
                 
-                if(l > r) i2++;
-                else i1--;
+                if(l > r && i2 + 1 < N) r += cookie[++i2];
+                else if(i1 - 1 >= 0) l += cookie[--i1];
+                else break;
             }
         }
         return max;
