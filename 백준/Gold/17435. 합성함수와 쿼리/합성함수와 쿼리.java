6a8b602817e2bd6f1d33ccb5e0f10aaee0235716
Aloe-droid <103430477+Aloe-droid@users.noreply.github.com>
2025-03-23 15:11:04 +0900

    [Gold I] Title: 합성함수와 쿼리, Time: 1436 ms, Memory: 103128 KB -BaekjoonHub

---
+++ 백준/Gold/17435. 합성함수와 쿼리/합성함수와 쿼리.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    static int N, Q;
+    static int[][] table;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        N = Integer.parseInt(br.readLine());
+        table = new int[N + 1][21];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 1; i <= N; i++) table[i][0] = Integer.parseInt(st.nextToken());
+        init();
+
+        Q = Integer.parseInt(br.readLine());
+        for(int i = 0 ; i < Q; i++) {
+            st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int x = Integer.parseInt(st.nextToken());
+            sb.append(query(n, x)).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    public static void init() {
+        for(int i = 1; i < 21; i++) {
+            for(int j = 1; j <= N; j++) {
+                table[j][i] = table[table[j][i - 1]][i - 1];
+            }
+        }
+    }
+
+    public static int query(int n, int x) {
+        if (n == 0) return x;
+
+        int k = 0;
+        while((int) Math.pow(2, k) <= n) k++;
+        k--;
+
+        int p = (int) Math.pow(2, k);
+        return query(n - p, table[x][k]);
+    }
+}
